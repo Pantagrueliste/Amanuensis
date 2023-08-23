cceb91da02efcf294d545bc000849fe1c008a8fd
ClementGodbarge <clem@clementgodbarges-macbook-pro.home>
2023-08-23 23:48:09 +0200

    word_normalization.py refactoring -- DWN1.1

--- modules/unicode_replacement.py
+++ modules/unicode_replacement.py
@@ -7,70 +7,70 @@ from rich.progress import Progress
 
 class UnicodeReplacement:
     def __init__(self, config):
-        # Extract the necessary settings from the config object and store them as separate attributes
+        self._initialize_config(config)
+        self.log = []
+
+
+    def _initialize_config(self, config):
         self.replacements_on = config.get('unicode_replacements', 'replacements_on')
         self.characters_to_delete = config.get('unicode_replacements', 'characters_to_delete')
         self.characters_to_replace = config.get('unicode_replacements', 'characters_to_replace')
         self.output_path = config.get('paths', 'output_path')
 
-        # Initialize the log
-        self.log = []
-
 
     def replace(self, input_file_path):
-        """
-        Applies Unicode replacements on the given file and saves the modified text
-        in a separate path specified in the configuration file.
-        """
-        with open(input_file_path, 'r', encoding='utf-8') as file:
-            lines = file.readlines()
+            """Applies Unicode replacements on the given file and saves the modified text."""
+            print(f"Processing file: {input_file_path}")  # Debug
+            with open(input_file_path, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
 
-        modified_lines = []
-        for line_num, line in enumerate(lines, 1):
+            modified_lines = [self._modify_line(line_num, line, input_file_path) for line_num, line in enumerate(lines, 1)]
+
+            self._write_output_file(input_file_path, modified_lines)
+
+
+    def _modify_line(self, line_num, line, input_file_path):
             modified_line = line
-
             if self.replacements_on:
-                # Delete specified characters
                 for char in self.characters_to_delete:
                     if char in modified_line:
                         self.log_change(input_file_path, line_num, char, "deleted")
                         modified_line = modified_line.replace(char, "")
 
-                # Apply specified replacements
                 for original, replacement in self.characters_to_replace.items():
                     if original in modified_line:
                         self.log_change(input_file_path, line_num, original, replacement)
                         modified_line = modified_line.replace(original, replacement)
 
-            modified_lines.append(modified_line)
+            return modified_line
 
-        # Prepare the output file path
-        output_path = self.output_path
-        file_name = os.path.basename(input_file_path)
-        output_file_path = os.path.join(output_path, file_name)
 
-        # Write the modified text to the output file
+    def _write_output_file(self, input_file_path, modified_lines):
+        output_file_path = os.path.join(self.output_path, os.path.basename(input_file_path))
         with open(output_file_path, 'w', encoding='utf-8') as file:
             file.writelines(modified_lines)
 
 
-    def log_change(self, file_path, line_num, original, replacement):
+    def log_change(self, input_file_path, line_num, original, replacement):
         """
         Log a replacement or deletion to the log list.
         """
+        print(f"Logging change: {original} -> {replacement} in {input_file_path} at line {line_num}")
         log_entry = {
             'timestamp': time(),
-            'file_name': os.path.basename(file_path),
+            'file_name': os.path.basename(input_file_path),
             'line_number': line_num,
             'original': original,
             'replacement': replacement
         }
         self.log.append(log_entry)
 
+
     def save_log(self):
         """
         Save the log list to a JSON file in the logs directory.
         """
+        print("Entering save_log method...") # debug
         # Determine the base directory of the app
         base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         log_dir = os.path.join(base_dir, 'logs')
@@ -80,9 +80,18 @@ class UnicodeReplacement:
             os.makedirs(log_dir)
 
         log_file_path = os.path.join(log_dir, 'replacements_log.json')
+        print(f"Log file path: {log_file_path}")
 
-        with open(log_file_path, 'w', encoding='utf-8') as file:
-            json.dump(self.log, file, ensure_ascii=False, indent=4)
+        if len(self.log) == 0:
+            print("No log entries to save.")
+        else:
+            print(f"First 5 log entries (if available): {self.log[:5]}")
+        try:
+            print("Log before saving:", self.log[:5])  # Debug
+            with open(log_file_path, 'w', encoding='utf-8') as file:
+                json.dump(self.log, file, ensure_ascii=False, indent=4)
+        except Exception as e:
+            print(f"Error saving log: {e}")
 
 
     def print_summary(self):
@@ -100,16 +109,13 @@ class UnicodeReplacement:
 
 
     def process_files(self, input_files):
-           """
-           Apply Unicode replacements on multiple files using multiprocessing.
-           """
-           with Progress() as progress:
-               task = progress.add_task("[green]Processing...", total=len(input_files))
-               with Pool() as pool:
-                   for _ in pool.imap_unordered(self.replace, input_files):
-                       progress.update(task, advance=1)
+            """Apply Unicode replacements on multiple files using multiprocessing."""
+            with Progress() as progress:
+                task = progress.add_task("[green]Processing...", total=len(input_files))
+                with Pool() as pool:
+                    for _ in pool.imap_unordered(self.replace, input_files):
+                        progress.update(task, advance=1)
 
-           self.save_log()
-           self.print_summary()
-
-           print(f"Processed {len(input_files)} files. Check the replacements_log.json for details.")
+            self.save_log()
+            self.print_summary()
+            print(f"Processed {len(input_files)} files. Check the replacements_log.json for details.")
