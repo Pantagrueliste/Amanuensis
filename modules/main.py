cdbdbe1f14dfcf8f1e29a516508ac5b628b64804
Clément Godbarge <cag437@nyu.edu>
2023-08-25 00:00:46 +0200

    formatting
    formatting

--- modules/main.py
+++ modules/main.py
@@ -22,7 +22,8 @@ from conflict_resolver import ConflictResolver
 from rich.logging import RichHandler
 from rich.progress import Progress
 
-nltk.download('wordnet')
+nltk.download("wordnet")
+
 
 class Amanuensis:
     def __init__(self):
@@ -36,23 +37,22 @@ class Amanuensis:
         """
         Main method to start word normalization process.
         """
-        if self.config.get('unicode_replacements', 'replacements_on'):
+        if self.config.get("unicode_replacements", "replacements_on"):
             self.run_unicode_replacement()
 
-        input_directory = self.config.get('paths', 'input_path')
+        input_directory = self.config.get("paths", "input_path")
         self.run_word_normalization()
 
         print("Resolving conflicts between Machine and User Solutions...")
         self.conflict_resolver.detect_and_resolve_conflicts()
         print("Conflict Resolution Complete.")
 
-
     def run_unicode_replacement(self):
         """
         Perform Unicode replacements on all text files in the input directory.
         """
         print("Starting Unicode Replacement...")
-        input_path = self.config.get('paths', 'input_path')
+        input_path = self.config.get("paths", "input_path")
         file_paths = self.get_all_text_files(input_path)
 
         print("Launch process_files.")
@@ -61,16 +61,14 @@ class Amanuensis:
         self.unicode_replacement.save_log()
         print("Unicode Replacement Complete.")
 
-
     def run_word_normalization(self):
         """
         Perform Dynamic Word Normalization on all text files in the input directory.
         """
         print("Starting Dynamic Word Normalization...")
-        input_directory = self.config.get('paths', 'input_path')
+        input_directory = self.config.get("paths", "input_path")
         self.word_normalization.preprocess_directory(input_directory)
         print("Dynamic Word Normalization Complete.")
-
 
     def get_all_text_files(self, dir_path):
         """
@@ -79,11 +77,12 @@ class Amanuensis:
         text_files = []
         for subdir, _, files in os.walk(dir_path):
             for file in files:
-                if file.endswith('.txt'):
+                if file.endswith(".txt"):
                     text_files.append(os.path.join(subdir, file))
         return text_files
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         amanuensis = Amanuensis()
         amanuensis.run()
