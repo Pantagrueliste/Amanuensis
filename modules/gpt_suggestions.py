72bf26a1153dd230272b3e062a30949d55e784db
Clément Godbarge <cag437@nyu.edu>
2023-08-27 21:53:14 +0200

    Improved DWN1.2

--- modules/gpt_suggestions.py
+++ modules/gpt_suggestions.py
@@ -1,9 +1,36 @@
-import asyncio
+"""
+This file contains the GPTSuggestions class, which is responsible for retrieving suggestions from GPT-4.
+This class is only instantiated if the user has enabled the GPT-4 suggestions in the config.toml file.
+It sends the context to the GPT-4 API and retrieves the suggestions as to what should replace the $ symbol.
+The suggestion is then displayed to the user.
+"""
+import openai
+
 
 class GPTSuggestions:
     def __init__(self, config):
         self.config = config
+        self.gpt = openai.Completion()
 
-    async def suggest(self):
-        # Perform the GPT suggestions functionality
-        pass
+    def get_suggestion(self, context):
+        """
+        Get a suggestion from GPT-4.
+        """
+        suggestion = self.gpt.get_top_reply(context)
+        return suggestion
+
+    def print_suggestion(self, context, suggestion):
+        """
+        Print the suggestion to the user.
+        """
+        print("\n\n")
+        print(f"[bold]GPT Suggestion:[/bold] {suggestion}")
+        print("\n\n")
+
+    def get_and_print_suggestion(self, context):
+        """
+        Get and print a suggestion from GPT-4.
+        """
+        suggestion = self.get_suggestion(context)
+        self.print_suggestion(context, suggestion)
+        return suggestion
