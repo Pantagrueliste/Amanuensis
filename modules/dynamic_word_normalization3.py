585c2741f29e6d1ef091b2c59b72b5a224d3219c
Clément Godbarge <cag437@nyu.edu>
2023-08-29 19:58:25 +0200

    daily update
    DWN2 first draft complete.

--- modules/dynamic_word_normalization3.py
+++ modules/dynamic_word_normalization3.py
@@ -1,31 +1,53 @@
+import os
 import json
-
+import shutil
+import tempfile
+from config import Config  # Assuming Config class is in a module named config
+from concurrent.futures import ThreadPoolExecutor
 
 class DynamicWordNormalization3:
-    def __init__(
-        self,
-        user_solution_path="data/user_solution.json",
-        machine_solution_path="data/machine_solution.json",
-    ):
-        # Load user solutions and machine solutions
-        self.user_solutions = self.load_solutions(user_solution_path)
-        self.machine_solutions = self.load_solutions(machine_solution_path)
+    def __init__(self):
+        self.config = Config()  # Initialize Config to get settings
 
-        # # Resolve inconsistencies
-        # self.resolve_inconsistencies()
+    def atomic_json_update(self, data, file_path):
+        with tempfile.NamedTemporaryFile('w', delete=False) as temp_file:
+            json.dump(data, temp_file)
+            temp_file.flush()
+        os.replace(temp_file.name, file_path)
 
-    def load_solutions(self, file_path):
-        """Load solutions from a JSON file."""
+    def handle_problematic_files_with_atomic_update(self, sorted_problematic_ratios, user_solution_json_path):
         try:
-            with open(file_path, "r", encoding="utf-8") as file:
-                return json.load(file)
+            with open(user_solution_json_path, 'r') as f:
+                current_data = json.load(f)
         except FileNotFoundError:
-            raise FileNotFoundError(f"Solutions file '{file_path}' not found.")
+            current_data = {}
 
-    def resolve_inconsistencies(self):
-        """Compare US and MS, and resolve any inconsistencies by prioritizing US."""
-        for aw, us in self.user_solutions.items():
-            ms = self.machine_solutions.get(aw)
-            if ms and ms != us:
-                # Inconsistency found, prioritize US
-                self.machine_solutions[aw] = us
+        for file_name, (count, ratio) in sorted_problematic_ratios.items():
+            print(f"File: {file_name}, Problematic Passages: {count}, Ratio: {ratio:.2f}%")
+
+            simulated_user_input = 'f'  # Replace with actual user input
+
+            if simulated_user_input.lower() == 'd':
+                print(f"Discarded {file_name}")
+                continue
+            elif simulated_user_input.lower() == 'f':
+                new_data = {file_name: {"line": count, "solution": "new_solution"}}
+                current_data.update(new_data)
+                self.atomic_json_update(current_data, user_solution_json_path)
+                print(f"Fix applied for {file_name}")
+            else:
+                print("Invalid option. Skipping to next file.")
+
+    def copy_file(self, src, dest):
+        with open(src, 'rb') as fsrc, open(dest, 'wb') as fdest:
+            shutil.copyfileobj(fsrc, fdest)
+
+    def final_processing(self, files_to_copy, dest_folder):
+        with ThreadPoolExecutor() as executor:
+            futures = []
+            for src_file in files_to_copy:
+                dest_file = os.path.join(dest_folder, os.path.basename(src_file))
+                futures.append(executor.submit(self.copy_file, src_file, dest_file))
+
+            for future in futures:
+                future.result()
