0f60b2a58b227db949614efe3f9291d42c5e70bd
Clément Godbarge <cag437@nyu.edu>
2023-08-31 23:33:32 +0200

    bug fixes
    bug fixes

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 python3 -m venv env
 source env/bin/activate
 pip install --upgrade pip
-pip install toml Pool nltk rich Progress art text2art Levenshtein prompt_toolkit
+pip install toml Pool nltk rich openai atomicwrites Progress art text2art Levenshtein prompt_toolkit
 
 python modules/main.py
