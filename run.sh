9704f3b5a7f2fa1da294ce41caf9403ecd29b1e8
Clément Godbarge <cag437@nyu.edu>
2023-09-02 20:25:57 +0200

    daily update
    Bug fixes and improved documentation.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-python -m venv env
+python3 -m venv env
 source env/bin/activate
 pip install --upgrade pip
 pip install toml Pool nltk rich openai atomicwrites Progress art text2art Levenshtein prompt_toolkit
-python -m nltk.downloader wordnet
+python3 -m nltk.downloader wordnet
 
 python modules/main.py
