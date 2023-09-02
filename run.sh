fed6aaebaa87a7cd1e28a6d8273006b47d3e4fde
Clément Godbarge <cag437@nyu.edu>
2023-09-02 12:02:05 +0200

    daily update

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-python3 -m venv env
+python -m venv env
 source env/bin/activate
 pip install --upgrade pip
 pip install toml Pool nltk rich openai atomicwrites Progress art text2art Levenshtein prompt_toolkit
+python -m nltk.downloader wordnet
 
 python modules/main.py
