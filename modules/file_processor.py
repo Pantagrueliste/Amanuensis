58ba692d320203921d7491b712761e080ee4d1de
Clément Godbarge <cag437@nyu.edu>
2023-09-04 16:16:58 +0200

    daily update
    bug fixes

--- modules/file_processor.py
+++ modules/file_processor.py
@@ -1,5 +1,9 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 import os
+import json
+import logging
+
+from atomic_update import atomic_write_text
 from config import Config
 
 class FastFileProcessor:
@@ -8,24 +12,72 @@ class FastFileProcessor:
         self.output_path = self.config.get("paths", "output_path")
         self.user_solution_file = user_solution_file
         self.machine_solution_file = machine_solution_file
-        # TODO: Load user and machine solutions
+        self.user_solutions = self.load_solutions(file_path=self.user_solution_file)
+        self.machine_solutions = self.load_solutions(file_path=self.machine_solution_file)
 
-    def load_solutions(self):
-        # TODO: Load solutions from user_solution.json and machine_solution.json
-        pass
+    def load_solutions(self, file_path: str) -> dict:
+        """Load solutions from a JSON file."""
+        try:
+            with open(file_path, 'r') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return {}
 
-    def process_file(self, file_path):
-        # TODO: Implement the logic to process a single file
-        # E.g., read the file, apply the solutions, and save the modified content
-        pass
+    def apply_abbreviations(self, text: str) -> str:
+        for original, replacement in self.user_solutions.items():
+            text = text.replace(original, replacement)
+        for original, replacement in self.machine_solutions.items():
+            text = text.replace(original, replacement)
+        return text
+
+    def process_file(self, file_path: str):
+        """Implement the logic to process a single file."""
+        try:
+            with open(file_path, 'r') as f:
+                content = f.read()
+
+            content = self.apply_abbreviations(content)
+
+            output_file_path = os.path.join(self.output_path, os.path.basename(file_path))
+            atomic_write_text(file_path=output_file_path, data=content)
+
+        except Exception as e:
+            logging.error(f"Failed to process {file_path}: {e}")
+
+    def process_files_in_parallel(self, file_list: list, num_workers: int):
+        """Process multiple files in parallel."""
+        with Pool(num_workers) as p:
+            p.map(FastFileProcessor().process_file, file_list)
+
 
     def parallel_process_files(self, files):
         with Pool() as pool:
             pool.map(self.process_file, files)
 
     def run(self):
-        # TODO: Identify the list of files to be processed
-        files_to_process = []
+        # Load your AW mappings from JSON files
+        user_solutions = self.user_solutions
+        machine_solutions = self.machine_solutions
 
-        # Perform parallel file processing
-        self.parallel_process_files(files_to_process)
+        # Create a set of all AWs to speed up lookup
+        all_AWs = set(user_solutions.keys()).union(set(machine_solutions.keys()))
+
+        # Get the list of all files in the directory specified in config.toml
+        input_path = self.config.get("paths", "input_path")
+        files_to_process = [os.path.join(input_path, f) for f in os.listdir(input_path) if os.path.isfile(os.path.join(input_path, f))]
+
+        for file_path in files_to_process:
+            with open(file_path, 'r') as f:
+                content = f.read()
+
+            # Check if this file contains any AWs
+            if any(aw in content for aw in all_AWs):
+                # Apply replacements
+                for original, replacement in user_solutions.items():
+                    content = content.replace(original, replacement)
+                for original, replacement in machine_solutions.items():
+                    content = content.replace(original, replacement)
+
+                # Save the modified content
+                output_file_path = os.path.join(self.output_path, os.path.basename(file_path))
+                atomic_write_text(file_path=output_file_path, data=content)
