a3afb81d2872aadd71ecd41739db1906cf9d985d
Clement Godbarge <cag437@nyu.edu>
2024-02-20 16:27:42 +0000

    daily update

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ python3 -m venv env
 source env/bin/activate
 
 pip install --upgrade pip
-pip install toml Pool nltk rich openai atomicwrites Progress art text2art Levenshtein prompt_toolkit orjson pyahocorasick
+pip install toml Pool rich openai atomicwrites Progress art ijson text2art Levenshtein prompt_toolkit orjson pyahocorasick
+python -m nltk.downloader wordnet
 
 python modules/main.py
